72433d9a885581d9945ab23379e1e89947e3ac43
jonrichmond <jrichmond124@gmail.com>
2021-10-18 00:19:56 -0400

    enhancement: Route 53 records

--- main.tf
+++ main.tf
@@ -33,6 +33,28 @@ resource "aws_route53_zone" "primary" {
   name = var.domain_name
 }
 
+resource "aws_route53_record" "root-domain" {
+  zone_id = aws_route53_zone.primary.id
+  name    = var.domain_name
+  type    = "A"
+  alias {
+    name = "s3-website-us-east-1.amazonaws.com"
+    zone_id                = aws_s3_bucket.secondary-app-bucket.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.primary.id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+  alias {
+    name = "s3-website-us-east-1.amazonaws.com"
+    zone_id                = aws_s3_bucket.secondary-app-bucket.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
 resource "aws_s3_bucket" "log-bucket" {
   bucket = "logs.${var.domain_name}"
   acl    = "log-delivery-write"
